82bc5851ffe6aa2d08b224f941d46392aea9b1b0
Gus Ortiz <guspidey@gmail.com>
2015-06-01 06:28:36 -0500

    Create map with terrain, add sprites to cars

--- demo/src/js/road.js
+++ demo/src/js/road.js
@@ -22,26 +22,42 @@ Crafty.scene('SemaphoreDemo', function () {
             car1 : {
                 x : 0,
                 y : 330,
-                z : 90,
+                z : 3,
                 w : 80,
                 h : 40 
             },
             car2 : {
                 x : 250,
                 y : 0,
-                z : 990,
+                z : 3,
                 w : 40,
                 h : 80
             }
         },
-        car1Attrs = {},
-        car2Attrs = {},
         car1,
         car2;
 
         var mapBuilder = Crafty.e("2D, Canvas, TiledMapBuilder").setMapDataSource(mapsrc);
 
         mapBuilder.createWorld(function (map){
+            var lights = [];
+
+            entities.vStopLights  = mapBuilder.getEntitiesInLayer('v-stop-lights');
+            entities.hStopLights  = mapBuilder.getEntitiesInLayer('h-stop-lights');
+            entities.vGreenLights = mapBuilder.getEntitiesInLayer('v-green-lights');
+            entities.hGreenLights = mapBuilder.getEntitiesInLayer('h-green-lights');
+            entities.semaphores   = mapBuilder.getEntitiesInLayer('semaphores');
+
+            lights = entities.vStopLights.concat(entities.hStopLights, entities.vGreenLights, entities.hGreenLights);
+
+            lights.forEach(function (light) {
+                light.z = 5;
+            });
+
+            entities.semaphores.forEach(function (sem) {
+                sem.z = 4;
+            });
+
         });
 
         car1 = Crafty.e('carH, Car, car_h');
